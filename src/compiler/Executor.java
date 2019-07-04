f7b855f3d30ea8e8bb92185d98d2f2fed520da5f
Jordan Pitlor <jordan@jordanpitlor.com>
2019-07-04 16:23:07 -0400

    executing cleans up the temp files

--- src/compiler/Executor.java
+++ src/compiler/Executor.java
@@ -3,37 +3,36 @@ package compiler;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
 
 import static java.nio.file.Files.*;
 
 public class Executor {
-	private Path tmpDirectory = createTempDirectory("jcc-");
-
-	public Executor() throws IOException {
-	}
-
-	private String getPath(String f) {
-		return new File(tmpDirectory.toFile(), f).getAbsolutePath()
+	private String path(Path parent, String f) {
+		return new File(parent.toFile(), f).getAbsolutePath()
 				.replace("C:\\", "/mnt/c/")
 				.replace("\\", "/");
 	}
 
-	public void Execute(String assembly) throws InterruptedException {
+	private void cleanUp(Path tmpDir) {
+		for (File file : tmpDir.toFile().listFiles()) file.delete();
+		tmpDir.toFile().delete();
+	}
+	public void Execute(String assembly) {
 		try {
-			File tmpFile = File.createTempFile("j-code-", ".s", tmpDirectory.toFile());
-//			tmpFile.deleteOnExit();
-//			tmpDirectory.toFile().deleteOnExit();
-
+			Path tmpDir = createTempDirectory("jcc-");
+			File tmpFile = File.createTempFile("j-code-", ".s", tmpDir.toFile());
 			BufferedWriter writer = new BufferedWriter(new FileWriter(tmpFile));
 			writer.write(assembly);
 			writer.close();
 
 			new ProcessBuilder().command("bash", "-c",
-				"gcc -o " + getPath("j-binary") + " " + getPath(tmpFile.getName()) + " && " + getPath("j-binary"))
+				String.format("gcc -o %s %s && %s",
+						path(tmpDir, "j-binary"), path(tmpDir, tmpFile.getName()), path(tmpDir, "j-binary")))
 				.inheritIO().start().waitFor();
-		} catch (IOException e) {
+
+			cleanUp(tmpDir);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
